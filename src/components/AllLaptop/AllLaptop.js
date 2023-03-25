c70a94a9210b591f46a991092a3ccd748259e5cd
Rayhan Shorker <raihanshorker56@gmail.com>
2023-03-26 02:08:28 +0600

    added add to cart btn

--- src/components/AllLaptop/AllLaptop.js
+++ src/components/AllLaptop/AllLaptop.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const AllLaptop = (props) => {
-    const {name,price}=props.laptop;
+    const {id,name,price}=props.laptop;
+    const addToCart =(id)=>{
+        console.log("item added",id);
+    }
     return (
         <div style={{border:"2px solid salmon",borderRadius:"20px",margin:"20px",padding:"20px",background:"lightblue"}}>
              <h4>Brand:{name}</h4>
             <p>Price:{price}</p>
+            <button onClick={()=>addToCart(id)}>Add to cart</button>
         </div>
     );
 };
--- src/components/AllMobile/AllMobile.js
+++ src/components/AllMobile/AllMobile.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const AllMobile = (props) => {
     const {id,name,price}=props.mobile;
+    const addToCart =(id)=>{
+        console.log("item added",id);
+    }
     return (
         <div style={{border:"2px solid salmon",borderRadius:"20px",margin:"20px",padding:"20px",background:"gray"}}>
             <h4>Brand:{name}</h4>
             <p>Price:{price}</p>
+            <button onClick={()=>addToCart(id)}>Add to cart</button>
         </div>
     );
 };
