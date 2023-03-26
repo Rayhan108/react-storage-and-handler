e093cc5d6cd86e789057ea3db1b2d5fe5698b565
Rayhan Shorker <raihanshorker56@gmail.com>
2023-03-26 19:32:05 +0600

    added remove and delete handler

--- src/components/AllLaptop/AllLaptop.js
+++ src/components/AllLaptop/AllLaptop.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { addToDb } from '../Utilities/FakeDb';
+import { addToDb, deleteShopingCart, removeItemFromDb} from '../Utilities/FakeDb';
 
 const AllLaptop = (props) => {
     const {id,name,price}=props.laptop;
     const addToCart =(id)=>{
       addToDb(id)
     }
+    const removeFromCart=id=>{
+       
+      removeItemFromDb(id);
+    }
+    const deletCart=()=>{
+      deleteShopingCart()
+    }
     return (
         <div style={{border:"2px solid salmon",borderRadius:"20px",margin:"20px",padding:"20px",background:"lightblue"}}>
              <h4>Brand:{name}</h4>
             <p>Price:{price}</p>
             <button onClick={()=>addToCart(id)}>Add to cart</button>
+            <button onClick={()=>removeFromCart(id)}>Remove</button>
+            <button onClick={deletCart}>Delete Cart</button>
         </div>
     );
 };
--- src/components/Utilities/FakeDb.js
+++ src/components/Utilities/FakeDb.js
@@ -11,6 +11,7 @@ if(storedCart){
     // add quantity    1st step
     const quantity = shopingCart[id];
     if(quantity){
+    //   এখানে parseInt(quantity) না দিলেউ হবে কারন উপরে JSON parse করা হয়েছে  
        const newQuantity =quantity+1;
        shopingCart[id]=newQuantity;
     }else{
@@ -20,4 +21,24 @@ if(storedCart){
     localStorage.setItem('shoping-cart',JSON.stringify(shopingCart));
 
 }
-export {addToDb}
+       
+
+
+const removeItemFromDb =id=>{
+    const storedCart =localStorage.getItem('shoping-cart');
+    if(storedCart){
+        const shopingCart = JSON.parse(storedCart);
+        if(id in shopingCart){
+            delete shopingCart[id];
+            localStorage.setItem('shoping-cart',JSON.stringify(shopingCart));
+        }
+    }
+}
+
+const deleteShopingCart =()=>{
+    localStorage.removeItem('shoping-cart');
+}
+export {
+    addToDb,
+ removeItemFromDb,
+deleteShopingCart}
